61dc7a8571331ba969cbbf7527b1978157f801d7
andrlazarev <andrey.lazarev@indriver.com>
2022-12-30 23:31:32 +0300

    Lesson 4, task 1

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -525,6 +525,139 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void testCheckArticleAfterDeletedArticleInFoldier()
+    {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Cannot find element SKIP",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find element 'Search Wikipedia'",
+                5
+        );
+
+        String search_line = "Java";
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_container"),
+                search_line,
+                "Cannot find element search input",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text='Object-oriented programming language']"),
+                "Cannot find element 'Object-oriented programming language' toping searching" + search_line,
+                15
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),
+                "Cannot find element 'Save'",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot find element for return",
+                5
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search field",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_container"),
+                "Appium",
+                "Cannot find element search input",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Automation for Apps')]"),
+                "Cannot find element 'Appium' in search",
+                15
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),
+                "Cannot find element 'Save'",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot find element for return",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@bounds='[0,66][154,220]']"),
+                "Cannot find element for return",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/nav_tab_reading_lists"),
+                "Cannot find element 'Saved'",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/recycler_view"),
+                "Cannot find element 'Saved'",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Appium')]"),
+                "Cannot find Article in saved",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/page_save"),
+                "Cannot find element 'Save'",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Remove from Saved')]"),
+                "Cannot find element 'Remove from Saved'",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot find element for return",
+                5
+        );
+
+        waitForElementNotPresent(
+                By.xpath("//*[contains(@text,'Appium')]"),
+                "Element 'Appium' don't removed from Saved article",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Java (programming language)')]"),
+                "Cannot find Article in saved",
+                5
+        );
+
+        assertElementHasText(
+                By.xpath("//*[contains(@text,'Java (programming language)')]"),
+                "Java (programming language)",
+                "Cannot find title this article"
+        );
+    }
+
     private WebElement waitForElementPresent(By by, String errorMessage, long timeInSeconds)
     {
         WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
