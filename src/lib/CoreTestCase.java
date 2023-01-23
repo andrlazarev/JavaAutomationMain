636c5d00e381248c291b98046b0668f0007ea95e
andrlazarev <andrey.lazarev@indriver.com>
2023-01-23 22:27:14 +0300

    method for choice driver platform

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -2,6 +2,7 @@ package lib;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -22,8 +23,7 @@ public class CoreTestCase extends TestCase {
     {
         super.setUp();
 
-        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        driver = getPlatformDriver();
 
         this.rotateScreenPortrait();
     }
@@ -75,5 +75,22 @@ public class CoreTestCase extends TestCase {
             throw new Exception("Cannot get run platform from env variable. Platform value" + platform);
         }
         return capabilities;
+    }
+
+
+    private AppiumDriver getPlatformDriver() throws Exception
+    {
+        String platform = System.getenv("PLATFORM");
+
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        } else if (platform.equals(PLATFORM_IOS)) {
+            driver = new IOSDriver(new URL(AppiumURL), capabilities);
+        } else {
+            throw new Exception("Cannot get run platform from env variable. Platform value" + platform);
+        }
+        return driver;
     }
 }
